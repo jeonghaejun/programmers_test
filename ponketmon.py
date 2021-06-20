44e0ba7451d9b77d70465dd72e2775d7c390aeb5
wjdgownsll <wjdgownsll@gmail.com>
2021-06-20 18:08:34 +0900

    edit ponketmon

--- ponketmon.py
+++ ponketmon.py
@@ -1,10 +1,14 @@
 def solution(nums):
-    
-    N=len(nums)/2
-    ponketmon=list(set(nums))
-    if len(ponketmon)>=N:
-        answer=N
+
+    N = len(nums)/2
+    ponketmon = list(set(nums))
+    if len(ponketmon) >= N:
+        answer = N
     else:
-        answer=len(ponketmon)
-    
+        answer = len(ponketmon)
+
     return answer
+
+# 둘중의 작은숫자가 폰켓몬 선택 개수 획기적이군!!
+def solution(ls):
+    return min(len(ls)/2, len(set(ls)))
