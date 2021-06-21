2802c3777d1a846fc31a7e9f9d01a2eba8f59610
wjdgownsll <wjdgownsll@gmail.com>
2021-06-21 16:13:22 +0900

    k_number

--- k_number.py
+++ k_number.py
@@ -2,24 +2,21 @@
 
 def solution(array, commands):
     answer = []
-    x = []
+
     for i, j, k in commands:
+        x = array[int(i-1):int(j)]
+        x.sort()
+        answer.append(x[int(k-1)])
+    return answer
 
-        if i != j:
-            x = array[int(i-1):int(j-1)]
-            print(x)
-        else:
-            x = array[int(i-1)]
-            print(x)
 
-    #     y=x.sort()
-    #     y = x[int(k-1)]
-    #     # print(y)
-    #     answer.append(y)
-    # return answer
+# map, lombda이용 겁나 간략하네
+
+def solution(array, commands):
+    return list(map(lambda x: sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
 
 
 array = [1, 5, 2, 6, 3, 7, 4]
 commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 
-solution(array, commands)
+print(solution(array, commands))
