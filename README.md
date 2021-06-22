9e7d38793391fd4fe4ab82b6703305af7a2912aa
wjdgownsll <wjdgownsll@gmail.com>
2021-06-22 13:46:25 +0900

    edit new_id

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 6. [폰켓몬](https://github.com/jeonghaejun/programmers_test/blob/master/ponketmon.py)
 7. [K번째 수](https://github.com/jeonghaejun/programmers_test/blob/master/k_number.py)
 8. [체육복](https://github.com/jeonghaejun/programmers_test/blob/master/greedy.py)
+9. [아이디 추천](https://github.com/jeonghaejun/programmers_test/blob/master/new_id.py)
--- new_id.py
+++ new_id.py
@@ -1,35 +1,41 @@
 import re
 
+
 def solution(new_id):
     # 1단계
     new_id = new_id.lower()
     # 2단계
     new_id = re.sub(
         '[\~\!\@\#\$\%\^\&\*\(\)\=\+\[\{\]\}\:\?\,\<\>\/]', '', new_id)
+
+    # 2단계 정규식 없이짜기
+    # for c in new_id:
+    #     if c.isalpha() or c.isdigit() or c in ['-', '_', '.']:
+    #         answer += c
     # 3단계
     while '..' in new_id:
         new_id = new_id.replace('..', '.')
     # 4단계
     if new_id[0] == '.':
-        new_id=new_id.replace('.','',1)
+        new_id = new_id.replace('.', '', 1)
 
-    if new_id[len(new_id)-1] == '.':
-        new_id=list(new_id)
-        new_id[len(new_id)-1] = ''
-        new_id=''.join(new_id)
+    if new_id != '':
+        if new_id[-1] == '.':
+            new_id = new_id[:-1]
     # 5단계
-    new_id=new_id.strip()
-    print('start'+new_id+'end')
-
+    if new_id == '':
+        new_id = 'a'
     # 6단계
-    if len(new_id)>=16:
-        new_id=new_id[0:14]
-    
-    print('start'+new_id+'end')
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+
+    if new_id != '':
+        if new_id[-1] == '.':
+            new_id = new_id[:-1]
+    # 7단계
+    if len(new_id) <= 2:
+        while len(new_id) < 3:
+            new_id=new_id+new_id[-1]
 
     return new_id
 
-
-new_id = "...!@BaT#*..y.abcdefghijklm."
-
-print(solution(new_id))
