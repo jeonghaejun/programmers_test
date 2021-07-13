17f1e00413418bb4233b651e6495e02f4d5861c0
wjdgownsll <wjdgownsll@gmail.com>
2021-07-13 16:20:38 +0900

    21.07.13

--- sum_range_int.py
+++ sum_range_int.py
@@ -1,6 +1,8 @@
 def solution(a, b):
-    if a>b:
-        a,b=b,a
+    if a>b: a,b=b,a
     sum_list=list(range(a,b+1))
-    
     return sum(sum_list)
+
+def solution(a, b):
+    if a>b: a,b=b,a
+    return sum(range(a,b+1))
